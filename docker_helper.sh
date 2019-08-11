cc3b6b6302f3a59876a3610d11ac206578976adb
Tanmay Balwa <tanmay.balwa@gmail.com>
2019-08-11 12:05:41 +0530

    correct the docker usage info with dockerhub image

--- docker_helper.sh
+++ docker_helper.sh
@@ -2,7 +2,7 @@
 
 display_usage() { 
 	echo 'This container has to be run using docker run --env MACADDRESSIO_API_KEY=key <image_name> mac_vendor.py <MAC_ADDRESS> <OPTIONAL_PARAMS>'
-	echo 'Example: docker run --env MACADDRESSIO_API_KEY=$MY_API_KEY github.com/balwa/macaddress-python-client:latest mac_vendor.py E8-40-40-79-C8-60'
-    echo 'See Usage: docker run github.com/balwa/macaddress-python-client:latest mac_vendor.py -h'
+	echo 'Example: docker run --env MACADDRESSIO_API_KEY=$MY_API_KEY balwa/macaddress-python-client:latest mac_vendor.py E8-40-40-79-C8-60'
+    echo 'See Usage: docker run balwa/macaddress-python-client:latest mac_vendor.py -h'
 	}
 display_usage
